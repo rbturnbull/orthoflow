83810634e6dfbee71c463903a42e7310ac57e2e8
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-05-27 14:50:18 +1000

    :cop: Move input data into separate directory

--- tests/test_gbseqextractor.py
+++ tests/test_gbseqextractor.py
@@ -16,34 +16,41 @@ def test_gbseqextractor():
     with TemporaryDirectory() as tmpdir:
         workdir = Path(tmpdir) / "workdir"
         tests_dir = Path(__file__).parent
-        data_path = tests_dir/"gbseqextractor/data"
-        expected_path = tests_dir/"gbseqextractor/expected"
-        conda_dir = tests_dir/".tests-conda"
+        data_path = tests_dir / "test-data/data/"
+        results_path = workdir / "results/fasta"
+        expected_path = tests_dir / "test-data/results/fasta"
+        conda_dir = tests_dir / ".tests-conda"
 
         # Copy data to the temporary workdir.
         shutil.copytree(data_path, workdir)
+        # breakpoint()
+
+        until = "add_taxon"
 
         # dbg
         print("results/fasta/MH591079.cds.fasta", file=sys.stderr)
 
         # Run the test job.
-        sp.check_output([
-            "phyloflow", 
-            data_path,
-            "-f", 
-            "-j1",
-            "--keep-target-files",
-            "--conda-frontend",
-            "conda",
-            "--use-conda",
-            "--conda-prefix",
-            conda_dir,
-            "--directory",
-            workdir,
-        ])
+        sp.check_output(
+            [
+                "phyloflow",
+                "-f",
+                "-j1",
+                "--keep-target-files",
+                "--conda-frontend",
+                "conda",
+                "--use-conda",
+                "--conda-prefix",
+                conda_dir,
+                "--directory",
+                workdir,
+                # "--until",
+                # *until,
+            ]
+        )
 
         # Check the output byte by byte using cmp.
         # To modify this behavior, you can inherit from common.OutputChecker in here
-        # and overwrite the method `compare_files(generated_file, expected_file), 
+        # and overwrite the method `compare_files(generated_file, expected_file),
         # also see common.py.
-        common.OutputChecker(data_path, expected_path, workdir).check()
+        common.OutputChecker(data_path, expected_path, results_path).check()
