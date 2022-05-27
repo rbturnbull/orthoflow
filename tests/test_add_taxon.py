8ae640e65bb1e8ac4a13560c4b5a0cd2e680ad0d
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-05-27 15:15:17 +1000

    :cop: refactor test 'add_taxon'

--- tests/test_add_taxon.py
+++ tests/test_add_taxon.py
@@ -16,12 +16,18 @@ def test_add_taxon():
     with TemporaryDirectory() as tmpdir:
         workdir = Path(tmpdir) / "workdir"
         tests_dir = Path(__file__).parent
-        data_path = tests_dir / "add_taxon/data"
-        expected_path = tests_dir / "add_taxon/expected"
+        data_path = tests_dir / "test-data/results/fasta"
+        input_source = tests_dir / "test-data/data/input_sources.csv"
+        results_path = workdir / "results/taxon-added"
+        expected_path = tests_dir / "test-data/results/taxon-added"
         conda_dir = tests_dir / ".tests-conda"
 
         # Copy data to the temporary workdir.
-        shutil.copytree(data_path, workdir)
+        shutil.copytree(data_path, workdir / "results/fasta/")
+        shutil.copy(input_source, workdir)
+
+        # Target rule to run until
+        until = "translate"
 
         # dbg
         print("results/taxon-added/MH591079.cds.fasta", file=sys.stderr)
@@ -30,7 +36,6 @@ def test_add_taxon():
         sp.check_output(
             [
                 "phyloflow",
-                data_path,
                 "-f",
                 "-j1",
                 "--keep-target-files",
@@ -41,6 +46,8 @@ def test_add_taxon():
                 conda_dir,
                 "--directory",
                 workdir,
+                "--until",
+                until,
             ]
         )
 
@@ -48,4 +55,4 @@ def test_add_taxon():
         # To modify this behavior, you can inherit from common.OutputChecker in here
         # and overwrite the method `compare_files(generated_file, expected_file),
         # also see common.py.
-        common.OutputChecker(data_path, expected_path, workdir).check()
+        common.OutputChecker(data_path, expected_path, results_path).check()
