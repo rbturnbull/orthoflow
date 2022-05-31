a060ede6f578f875702f26fc5a63db53925fef4f
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-05-31 11:34:32 +1000

    :zap: adding alignment to make workflow target and adding test for alignment

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -1,4 +1,4 @@
 from .common import assert_expected
 
-# def test_mafft():
-#     assert_expected("results/alignment/alignment.fa")
+def test_mafft():
+    assert_expected("results/alignment/alignment.fa")
