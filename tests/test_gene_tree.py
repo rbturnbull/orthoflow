9ba622de1cf7259d9d453a0a310d7ccb12a84a63
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-07-28 23:36:13 +1000

    :cop: another test for gene tree module - can close #11

--- tests/test_gene_tree.py
+++ tests/test_gene_tree.py
@@ -1,8 +1,8 @@
-# def test_gene_tree_iqtree(run_workflow):
-#     w = run_workflow("results/gene_tree/iqtree/OG0000003/")
-#     w.assert_dir_exists()
-#     w.assert_cl()
-
+def test_gene_tree_iqtree(run_workflow):
+    w = run_workflow("results/gene_tree/iqtree/OG0000003/")
+    w.assert_dir_exists()
+    w.assert_glob_count("*.treefile", count=1)
+    
 
 def test_gene_tree_ascii(run_workflow):
     w = run_workflow("results/gene_tree/ascii/OG0000003.txt")
