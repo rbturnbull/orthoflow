33ac9358fa73141504347fcb0a5e5ef5c4c18f2d
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2022-09-09 16:18:29 +1000

    :abc: making test_supertree_ascii less strict

--- tests/test_supertree.py
+++ tests/test_supertree.py
@@ -7,8 +7,9 @@ def test_astral(run_workflow):
     
 def test_supertree_ascii(run_workflow):
     w = run_workflow("results/supertree/supertree_ascii.cds.txt", "--config", "infer_tree_with_protein_seqs=0")
-    w.assert_contains("| Dichotomosiphon_tube")
-    w.assert_contains("| Flabellia_peti")
+    breakpoint()
+    w.assert_contains(" Dichotomosiphon_tube")
+    w.assert_contains(" Flabellia_peti")
 
 
 def test_supertree_render(run_workflow):
