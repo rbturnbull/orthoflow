0c50ba940ca22daccdb7fa7879b9c7de8e08e5d5
Heroen Verbruggen <heroen.verbruggen@gmail.com>
2022-09-24 14:22:51 +1000

    :bug: removing problematic progress dependency from generate_orthosnap_input.py

--- orthoflow/workflow/scripts/generate_orthosnap_input.py
+++ orthoflow/workflow/scripts/generate_orthosnap_input.py
@@ -4,7 +4,6 @@ import shutil
 from pathlib import Path
 
 import typer
-from rich.progress import Progress, TextColumn
 
 
 def main(
@@ -21,23 +20,15 @@ def main(
     fasta_input_files = list(og_dir.glob('*.fa'))
     pattern = re.compile(r"(OG\d+)\.fa")
 
-    with Progress(*Progress.get_default_columns(), TextColumn("({task.completed})")) as progress:
-        task_files = progress.add_task("Processing...", total=len(fasta_input_files))
-        task_kept = progress.add_task("[green]Kept...", total=None)
-        task_skipped = progress.add_task("[red]Skipped...", total=None)
 
-        for fasta_file in fasta_input_files:
-            progress.update(task_files, advance=1)
-            fasta = fasta_file.read_text()
-            fasta = fasta.replace(';', '_')
-            nseq = fasta.count('>')
-            if nseq >= min_seqs:
-                (outdir / fasta_file.name).write_text(fasta)
-                og = pattern.search(str(fasta_file)).group(1)
-                shutil.copyfile(gt_dir / (og + "_tree.txt"), outdir / (og + ".nwk")) # can this be a softlink?
-                progress.update(task_kept, advance=1)
-            else:
-                progress.update(task_skipped, advance=1)
+    for fasta_file in fasta_input_files:
+        fasta = fasta_file.read_text()
+        fasta = fasta.replace(';', '_')
+        nseq = fasta.count('>')
+        if nseq >= min_seqs:
+            (outdir / fasta_file.name).write_text(fasta)
+            og = pattern.search(str(fasta_file)).group(1)
+            shutil.copyfile(gt_dir / (og + "_tree.txt"), outdir / (og + ".nwk")) # can this be a softlink?
 
 
 if __name__ == "__main__":
