83ad88283bc5f071488c335c89886442c8c8b8aa
Heroen Verbruggen <heroen.verbruggen@gmail.com>
2022-12-12 15:37:44 +1100

    handle incomplete CDS in extract_cds.py
    trims start and/or end of CDS as appropriate to be a multiple of 3 bases and start at codon position 1

--- orthoflow/workflow/scripts/extract_cds.py
+++ orthoflow/workflow/scripts/extract_cds.py
@@ -24,15 +24,27 @@ def extract_cds(
                 if feat.type == "CDS":
                     feat_seq = feat.extract(seq)
                     gene = "NA"
+                    codon_start = 1
+                    seq_str = feat_seq.seq
                     try:
                         gene = feat.qualifiers["gene"][0]
                     except:
-                        logger.debug(f"Gene without gene name encountered in {infile}")
-                    print(">", seq.id, "|", gene, "\n", feat_seq.seq, sep="", file=fout)
+                        logger.debug(f"CDS without gene name encountered in {infile}")
+                    try:
+                        codon_start = int(feat.qualifiers["codon_start"][0])
+                    except:
+                        logger.debug(f"CDS without codon start position specification encountered in {infile}")
+                    if codon_start != 1:
+                        seq_str = seq_str[codon_start-1:len(seq_str)]
+                        logger.debug(f"CDS with codon start position different from 1 encountered in {infile}; sequence has been trimmed at start")
+                    modulo = len(seq_str) % 3
+                    if modulo != 0:
+                        seq_str = seq_str[0:len(seq_str)-modulo]
+                        logger.debug(f"CDS of length not divisible by 3 encountered in {infile}; sequence has been trimmed at end")
+                    print(">", seq.id, "|", gene, "\n", seq_str, sep="", file=fout)
                     counter += 1
 
     logger.debug(f"Extracted {counter} CDS from {infile} → {outfile}.")
-
 
 if __name__ == "__main__":
     typer.run(extract_cds)
