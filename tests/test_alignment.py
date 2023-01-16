72ac5ec1d067c3bb077e98f86202cb33f0f9142f
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-01-16 16:00:38 +1100

    :cop: updating test_thread_dna

--- tests/test_alignment.py
+++ tests/test_alignment.py
@@ -10,18 +10,6 @@ def test_get_cds_seq(run_workflow):
     w.assert_contains("ATGTTTGATTTTTTAAATAAACCATTAATAACAGAAAAAGCAACTCAACTTATTGAA")
 
 
-def test_thread_dna(run_workflow):
-    w = run_workflow("results/alignment/threaded_cds/OG0000048.cds.alignment.fa", "--config", "infer_tree_with_protein_seqs=0")
-    w.assert_contains(">Derbesia_sp_WEST4838\n")
-    w.assert_contains("TGATTTTATATGTTAAAACTGCAAGTCAAAATTCA----")
-
-
-def test_trim_alignments(run_workflow):
-    w = run_workflow("results/alignment/trimmed/OG0000003.trimmed.cds.alignment.fa", "--config", "infer_tree_with_protein_seqs=0")
-    w.assert_contains(">Derbesia_sp_WEST4838\n")
-    w.assert_contains("ATGACAGCTATTTTACAACGTCGCGAAAATACGACTTTATGGGC")
-
-
 def test_taxon_only(run_workflow):
     w = run_workflow("results/alignment/taxon_only/OG0000003.taxon_only.protein.alignment.fa")
     w.assert_contains(">Caulerpa_cliftonii_HV03798\n")
@@ -34,5 +22,19 @@ def test_taxon_only_cds(run_workflow):
     w.assert_contains(">Caulerpa_cliftonii_HV03798\n")
     w.assert_contains(">Avrainvillea_mazei_HV02664\n")
     w.assert_contains(">Chlorodesmis_fastigiata_HV03865\n")
+
+
+def test_thread_dna(run_workflow):
+    w = run_workflow("results/alignment/threaded_cds/OG0000002.cds.alignment.fa", "--config", "infer_tree_with_protein_seqs=0")
+    w.assert_contains(">Caulerpa_cliftonii_HV03798\n")
+    w.assert_contains("ATGTTTTGTAAACACAAAAAATCTAAAGATCAATTCTTTTTAACATGTGGA-----")
+
+
+def test_trim_alignments(run_workflow):
+    w = run_workflow("results/alignment/trimmed/OG0000003.trimmed.cds.alignment.fa", "--config", "infer_tree_with_protein_seqs=0")
+    w.assert_contains(">Derbesia_sp_WEST4838\n")
+    w.assert_contains("ATGACAGCTATTTTACAACGTCGCGAAAATACGACTTTATGGGC")
+
+
 
 
