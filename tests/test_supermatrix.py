fef9fccbd794fbe47fbfe8a5bf83f7ca3e2e6507
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-01-16 23:19:53 +1100

    :cop: there are now test_concatenate_alignments tests for both cds and protein

--- tests/test_supermatrix.py
+++ tests/test_supermatrix.py
@@ -1,14 +1,27 @@
-def test_alignment_summary(run_workflow):
+def test_alignment_summary_protein(run_workflow):
+    w = run_workflow("results/supermatrix/alignment_summary.protein.txt")
+    w.assert_re(r"\d+\s+Number of taxa")
+    w.assert_re(r"\d+\s+Alignment length")
+    w.assert_re(r"T\s+\d+")
+
+
+def test_alignment_summary_cds(run_workflow):
     w = run_workflow("results/supermatrix/alignment_summary.cds.txt", "--config", "infer_tree_with_protein_seqs=0")
     w.assert_re(r"\d+\s+Number of taxa")
     w.assert_re(r"\d+\s+Alignment length")
     w.assert_re(r"T\s+\d+")
 
 
-def test_concatenate_alignments(run_workflow):
+def test_concatenate_alignments_protein(run_workflow):
+    w = run_workflow("results/supermatrix/supermatrix.protein.fa")
+    w.assert_contains(">Avrainvillea_mazei_HV02664\n")
+    w.assert_contains("------MKIFEKLIYIVLILLIINL")
+
+
+def test_concatenate_alignments_cds(run_workflow):
     w = run_workflow("results/supermatrix/supermatrix.cds.fa", "--config", "infer_tree_with_protein_seqs=0")
     w.assert_contains(">Avrainvillea_mazei_HV02664\n")
-    w.assert_contains("ATGACAGCTATTTTACAACGTCGTTC")
+    w.assert_contains("------------------ATGAAAATTTTTGAAAAACTCATTT")
 
 
 def test_iqtree(run_workflow):
