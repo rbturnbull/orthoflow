f20f6bfd0ddf292ead461d5bdc419e148f643320
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-01-27 16:55:55 +1100

    :cop: adding tests for yaml, toml and json lists

--- tests/test_intake_utils.py
+++ tests/test_intake_utils.py
@@ -69,3 +69,79 @@ def test_input_toml():
     assert item.translation_table == 11
     assert item.taxon_string == "Bryopsis_plumosa"
 
+
+def test_input_json():
+    input_dictionary = intake_utils.create_input_dictionary(TEST_DATA_SMALL/"MH591084-truncated.gb.json")
+    assert len(input_dictionary) == 1
+    item = input_dictionary["MH591084-truncated"]
+    assert item.file == TEST_DATA_SMALL/"MH591084-truncated.gb"
+    assert item.data_type == "GenBank"
+    assert item.translation_table == 2 # testing override of the translation table read
+    assert item.taxon_string == "Flabellia_petiolata"
+
+
+def test_input_json_yaml_toml():
+    input_dictionary = intake_utils.create_input_dictionary(
+        [
+            TEST_DATA_SMALL/"MH591084-truncated.gb.json",
+            TEST_DATA_SMALL/"KY819064.cds.yaml",
+            TEST_DATA_SMALL/"NC_026795-truncated.toml",
+        ]
+    )
+    assert len(input_dictionary) == 3
+
+    item = input_dictionary["MH591084-truncated"]
+    assert item.file == TEST_DATA_SMALL/"MH591084-truncated.gb"
+    assert item.data_type == "GenBank"
+    assert item.translation_table == 2 # testing override of the translation table read
+    assert item.taxon_string == "Flabellia_petiolata"
+
+    item = input_dictionary["NC_026795-truncated"]
+    assert item.file == TEST_DATA_SMALL/"NC_026795-truncated.txt"
+    assert item.data_type == "GenBank"
+    assert item.translation_table == 11
+    assert item.taxon_string == "Bryopsis_plumosa"
+
+    item = input_dictionary["KY819064-cds"]
+    assert item.translation_table == 11
+    assert item.taxon_string == "Chlorodesmis_fastigiata_HV03865"
+    assert item.file == TEST_DATA_SMALL/"KY819064.cds.fasta"
+    assert item.data_type == "Fasta"
+
+
+def test_input_csv_test_data_small_toml():
+    input_dictionary = intake_utils.create_input_dictionary(TEST_DATA_SMALL/"input_sources.toml")
+    assert isinstance(input_dictionary, intake_utils.OrthoflowInputDictionary)
+    assert len(input_dictionary) == 7
+    assert list(input_dictionary.keys()) == ['KX808498-truncated', 'KY509313-truncated', 'MH591083-truncated', 'MH591084-truncated', 'MH591085-truncated', 'NC_026795-truncated', 'KY819064-cds']
+    
+    for data in input_dictionary.values():
+        data.translation_table == 11
+        assert data.file.exists()
+        assert data.file.parent == TEST_DATA_SMALL
+    
+
+def test_input_csv_test_data_small_json():
+    input_dictionary = intake_utils.create_input_dictionary(TEST_DATA_SMALL/"input_sources.json")
+    assert isinstance(input_dictionary, intake_utils.OrthoflowInputDictionary)
+    assert len(input_dictionary) == 7
+    assert list(input_dictionary.keys()) == ['KX808498-truncated', 'KY509313-truncated', 'MH591083-truncated', 'MH591084-truncated', 'MH591085-truncated', 'NC_026795-truncated', 'KY819064-cds']
+    
+    for data in input_dictionary.values():
+        data.translation_table == 11
+        assert data.file.exists()
+        assert data.file.parent == TEST_DATA_SMALL
+    
+
+def test_input_csv_test_data_small_yaml():
+    input_dictionary = intake_utils.create_input_dictionary(TEST_DATA_SMALL/"input_sources.yml")
+    assert isinstance(input_dictionary, intake_utils.OrthoflowInputDictionary)
+    assert len(input_dictionary) == 7
+    assert list(input_dictionary.keys()) == ['KX808498-truncated', 'KY509313-truncated', 'MH591083-truncated', 'MH591084-truncated', 'MH591085-truncated', 'NC_026795-truncated', 'KY819064-cds']
+    
+    for data in input_dictionary.values():
+        data.translation_table == 11
+        assert data.file.exists()
+        assert data.file.parent == TEST_DATA_SMALL
+    
+
