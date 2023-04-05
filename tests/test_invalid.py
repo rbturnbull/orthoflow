52e368e14fbb6ce4e57e371619ecd564ba8b858c
pellescholten <pelle.scholten@hotmail.com>
2023-04-05 15:59:06 +1000

    :zap: new method of checking validity and writing to warnings

--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -22,4 +22,10 @@ def test_gnb_invalid(run_workflow):
 
 def test_gnb_alphabet(run_workflow):
     with pytest.raises(CalledProcessError) as err:
-        w = run_workflow("results/intake/input_sources.csv", "--files", "alphabet.gb", expected_dir=invalid_expected_dir)
+        w = run_workflow("results/intake/input_sources.csv", "--files", "alphabet.gb", expected_dir=invalid_expected_dir )
+
+def test_warning(run_workflow):
+    w = run_workflow("results/intake/input_sources.csv", "--config", "ignore_non_valid_files=1", expected_dir=invalid_expected_dir )
+   
+# make test for asserting warning file exists and that it contains the right warning messages
+
