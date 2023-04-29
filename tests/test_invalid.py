f2935218a5b83b9b151b2a8d7a00e9663fa61033
pellescholten <pelle.scholten@hotmail.com>
2023-04-29 14:31:33 +1000

    :bug: moved failing test_error_no_alignments forward

--- tests/test_invalid.py
+++ tests/test_invalid.py
@@ -4,6 +4,10 @@ from subprocess import CalledProcessError
 
 invalid_expected_dir = Path(__file__).parent/"test-data-invalid"
 
+def test_error_no_alignments(run_workflow):
+    with pytest.raises(Exception) as err:
+        w = run_workflow("results/alignment/alignments_list_present.cds.txt", "--files", "input_sources.csv", "--config", "ignore_non_valid_files=1", "ortholog_min_seqs=20", "ortholog_min_taxa=20", expected_dir=invalid_expected_dir)
+
 def test_fasta_invalid_codon(run_workflow):
     with pytest.raises(CalledProcessError) as err:
         w = run_workflow("results/intake/input_sources.csv", "--files", "codons.fa", expected_dir=invalid_expected_dir )
@@ -37,8 +41,3 @@ def test_ignore_faulty_sequence(run_workflow):
     w = run_workflow("results/intake/cds/codons.cds.fa", "--files", "input_sources.csv", "--config", "ignore_non_valid_files=1", expected_dir=invalid_expected_dir)
     with pytest.raises(Exception) as err:
         w.assert_contains("emptysequence")
-
-def test_error_no_alignments(run_workflow):
-    with pytest.raises(CalledProcessError) as err:
-        w = run_workflow("results/alignment/alignments_list_present.cds.txt", "--files", "input_sources.csv", "--config", "ignore_non_valid_files=1", "ortholog_min_seqs=20", "ortholog_min_taxa=20", expected_dir=invalid_expected_dir)
-
