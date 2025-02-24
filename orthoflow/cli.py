6be75e43481a665e421b731fcb4e80ea677486dd
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-02-24 22:59:09 +1100

    :zap: adding bibtex command

--- orthoflow/cli.py
+++ orthoflow/cli.py
@@ -3,3 +3,10 @@ from pathlib import Path
 from snk_cli import CLI
 
 orthoflow = CLI(Path(__file__).parent)
+
+
+@orthoflow.app.command()
+def bibtex():
+    """ Print the BibTeX file for Orthoflow. """
+    bibfile = Path(__file__).parent / "workflow" / "bibs" / "orthoflow.bib"
+    print(bibfile.read_text())
