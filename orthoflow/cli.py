fba4da6dc169c8d13a9592cbaef05f1056e9dd04
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2025-02-24 23:07:01 +1100

    :zap: adding in helper commands

--- orthoflow/cli.py
+++ orthoflow/cli.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import typer
 from snk_cli import CLI
 
 orthoflow = CLI(Path(__file__).parent)
@@ -10,3 +10,15 @@ def bibtex():
     """ Print the BibTeX file for Orthoflow. """
     bibfile = Path(__file__).parent / "workflow" / "bibs" / "orthoflow.bib"
     print(bibfile.read_text())
+
+
+@orthoflow.app.command()
+def github():
+    """ Launch the Orthoflow GitHub page. """
+    typer.launch("https://github.com/rbturnbull/orthoflow")
+
+
+@orthoflow.app.command()
+def docs():
+    """ Launch the Orthoflow documentation. """
+    typer.launch("https://rbturnbull.github.io/orthoflow/")
